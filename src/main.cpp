c55619b7395ec3973063648d5fcf22b94be260a4
醉家男煮饺 <88431844@163.com>
2021-02-09 10:35:23 +0800

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,83 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+
+// ST7789 TFT module connections
+#define TFT_DC 5   // TFT DC  pin is connected to NodeMCU pin D1 (GPIO5)
+#define TFT_RST 16 // TFT RST pin is connected to NodeMCU pin D0 (GPIO16)
+#define TFT_CS 15  // TFT CS  pin is connected to NodeMCU pin D8 (GPIO15)
+// initialize ST7789 TFT library with hardware SPI module
+// SCK (CLK) ---> NodeMCU pin D5 (GPIO14)
+// MOSI(DIN) ---> NodeMCU pin D7 (GPIO13)
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+void tftPrintTest();
+float p = 3.1415926;
+
+void setup(void)
+{
+  Serial.begin(115200);
+
+  // if the display has CS pin try with SPI_MODE0
+  tft.init(240, 240, SPI_MODE2); // 改成你屏幕的大小
+
+  // if the screen is flipped, remove this command
+  tft.setRotation(2);
+
+  Serial.println(F("TFT init"));
+
+  uint16_t time = millis();
+  tft.fillScreen(ST77XX_BLACK);
+  time = millis() - time;
+
+  Serial.println(time, DEC);
+  delay(500);
+
+  // tft print function!
+  tftPrintTest();
+  delay(4000);
+}
+
+void loop()
+{
+}
+
+void tftPrintTest()
+{
+  tft.setTextWrap(false);
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setCursor(0, 30);
+  tft.setTextColor(ST77XX_RED);
+  tft.setTextSize(1);
+  tft.println("Hello World!");
+  tft.setTextColor(ST77XX_YELLOW);
+  tft.setTextSize(2);
+  tft.println("Hello World!");
+  tft.setTextColor(ST77XX_GREEN);
+  tft.setTextSize(3);
+  tft.println("Hello World!");
+  tft.setTextColor(ST77XX_BLUE);
+  tft.setTextSize(4);
+  tft.print(1234.567);
+  delay(1500);
+  tft.setCursor(0, 0);
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setTextSize(0);
+  tft.println("Hello World!");
+  tft.setTextSize(1);
+  tft.setTextColor(ST77XX_GREEN);
+  tft.print(p, 6);
+  tft.println(" Want pi?");
+  tft.println(" ");
+  tft.print(8675309, HEX); // print 8,675,309 out in HEX!
+  tft.println(" Print HEX!");
+  tft.println(" ");
+  tft.setTextColor(ST77XX_WHITE);
+  tft.println("Sketch has been");
+  tft.println("running for: ");
+  tft.setTextColor(ST77XX_MAGENTA);
+  tft.print(millis() / 1000);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.print(" seconds.");
+}
